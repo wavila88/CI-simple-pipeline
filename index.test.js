5544a1b75bdcbef5ec9423f42cfb866afd876564
William Avila <willocoav@example.com>
2022-10-18 14:56:45 -0500

    corrected

--- index.test.js
+++ index.test.js
@@ -1,8 +1,14 @@
-const sumar = require('./index');
+const sumar = require('./operations');
 const assert = require('assert');
+
+beforeAll(done => {
+  done()
+})
+
+
 describe('general test', () => {
   test('should sum', () => {
-    assert.equal(sumar(2,3), 5)
+    assert.equal(sumar(2,3), 5);
   })
   
 
